108a9f0fa7db91d05e6e21b58e008f2a6bdf6259
naranjox <182925606+bravex999@users.noreply.github.com>
2026-01-19 14:55:31 +0100

    listo

---
+++ ex00/Animal.cpp
@@ -0,0 +1,37 @@
+#include "Animal.hpp"
+
+Animal::Animal() : type("Animal")
+{
+    std::cout << "Animal Default Constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal& other)
+{
+    std::cout << "Animal Copy Constructor called" << std::endl;
+    *this = other;
+}
+
+Animal& Animal::operator=(const Animal& other)
+{
+    std::cout << "Animal Copy Assignment Operator called" << std::endl;
+    if (this != &other)
+    {
+        this->type = other.type;
+    }
+    return *this;
+}
+
+Animal::~Animal()
+{
+    std::cout << "Animal Destructor called" << std::endl;
+}
+
+void Animal::makeSound() const
+{
+    std::cout << "* Generic Animal Sound *" << std::endl;
+}
+
+std::string Animal::getType() const
+{
+    return this->type;
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,39 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+#include <iostream>
+
+int main()
+{
+    std::cout << "--- SUBJECT TESTS ---" << std::endl;
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    
+    i->makeSound();
+    j->makeSound();
+    meta->makeSound();
+
+    delete i;
+    delete j;
+    delete meta;
+
+    std::cout << "\n--- WRONG ANIMAL TESTS ---" << std::endl;
+    const WrongAnimal* wrongMeta = new WrongAnimal();
+    const WrongAnimal* wrongCat = new WrongCat();
+
+    std::cout << wrongCat->getType() << " " << std::endl;
+    
+    wrongCat->makeSound(); 
+    wrongMeta->makeSound();
+
+    delete wrongCat;
+    delete wrongMeta;
+
+    return 0;
+}
